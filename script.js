2dfd112445d87a94326888746d427c22f2ca508d
Wade Butler <wadebutler2@gmail.com>
2020-03-19 16:11:15 -0400

    added advanced tip calculator form

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// SIMPLE TIP CALCULATOR
 const getResultsSimple = () => {
     personalTip();
     personalCost();
@@ -17,7 +18,6 @@ const personalTip = () => {
 
     return dividedTip.toFixed(2);
 }
-
 // cost per person calculator
 const personalCost = () => {
     const cost = document.querySelector(".cost").value;
@@ -27,7 +27,6 @@ const personalCost = () => {
 
     return dividedCost.toFixed(2);
 }
-
 // gets the total cost per individual
 const individualTotal = () => {
     const cost = document.querySelector(".cost").value;
@@ -42,14 +41,13 @@ const individualTotal = () => {
 
     return soloTotal.toFixed(2);
 } 
-
 // display the results on page
 const results = () => {
     document.querySelector(".tipPer").innerHTML = personalTip();
     document.querySelector(".costPer").innerHTML = personalCost();
     document.querySelector(".totalPer").innerHTML = individualTotal();
+    
 }
-
 
 // SCREEN SWITCH CONTROLS
 // turn off the form and display results
@@ -57,20 +55,17 @@ const handleDisplay = () => {
     document.querySelector(".form").style.display = "none";
     document.querySelector(".results").style.display = "block";
 }
-
 // turn off the results and display the original form
 const goBack = () => {
     document.querySelector(".form").style.display = "block";
     document.querySelector(".results").style.display = "none";
     document.querySelector(".cost").value = "";
 }
-
 // go to simple tip calculator
 const simple = () => {
     document.querySelector(".advanced").style.display = "none";
     document.querySelector(".simple").style.display = "block";
 }
-
 // go to advanced tip calculator
 const advanced = () => {
     document.querySelector(".simple").style.display = "none";
