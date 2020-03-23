6a05cbdf45fec49527b47f1232118e44c82dad5e
Wade Butler <wadebutler2@gmail.com>
2020-03-23 14:12:30 -0400

    added advanced bill splitter functionality

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const getResultsSimple = () => {
     personalCost();
     individualTotal();
     results();
-    handleDisplay();
+    handleDisplaySimple();
 }
 
 //tip calculator
@@ -49,19 +49,106 @@ const results = () => {
     document.querySelector(".personDisplay").innerHTML = document.querySelector(".people").value;
 }
 
-// SCREEN SWITCH CONTROLS
-// turn off the form and display results
-const handleDisplay = () => {
+// ADVANCED TIP/TAX CALCULATOR
+const getResultsAdvanced = () => {
+    handleDisplayAdvanced();
+    provinceTax();
+    advTip();
+    costWithTax();
+    mealTotalAdvanced();
+    advancedSplit();
+    advResult();
+}
+
+const provinceTax = () => {
+    const cost = document.querySelector(".aCost").value;
+    const tax = document.querySelector(".aRegion").value;
+
+    const mealTax = cost * tax;
+    return mealTax;
+}
+
+const advTip = () => {
+    const cost = document.querySelector(".aCost").value;
+    const people = document.querySelector(".aPeople").value;
+    const tipPercent = document.querySelector(".aTip").value / 100;
+
+    const totalTip = cost * tipPercent;
+    const dividedTip = totalTip / people;
+
+    return dividedTip;
+}
+
+const costWithTax = () => {
+    const cost = document.querySelector(".aCost").value;
+    const tax = document.querySelector(".aRegion").value;
+
+    const mealTax = cost * tax;
+    const mealTotal = parseInt(cost) + parseInt(mealTax);
+    return mealTotal;
+}
+
+const mealTotalAdvanced = () => {
+    const cost = document.querySelector(".aCost").value;
+    const tax = document.querySelector(".aRegion").value;
+    const tipPercent = document.querySelector(".aTip").value / 100;
+
+    const mealTax = cost * tax;
+    const mealWithTax = parseInt(cost) + parseInt(mealTax);
+    const totalTip = cost * tipPercent;
+    const mealCost = mealWithTax + totalTip;
+    return mealCost;
+}
+
+const advancedSplit = () => {
+    const cost = document.querySelector(".aCost").value;
+    const tax = document.querySelector(".aRegion").value;
+    const tipPercent = document.querySelector(".aTip").value / 100;
+    const people = document.querySelector(".aPeople").value;
+
+    const mealTax = cost * tax;
+    const mealWithTax = parseInt(cost) + parseInt(mealTax);
+    const totalTip = cost * tipPercent;
+    const mealCost = mealWithTax + totalTip;
+    const split = mealCost / people
+    return split;
+}
+
+const advResult = () => {
+    document.querySelector(".mealTax").innerHTML = provinceTax();
+    document.querySelector(".advTip").innerHTML = advTip();
+    document.querySelector(".costTax").innerHTML = costWithTax();
+    document.querySelector(".aTotal").innerHTML = mealTotalAdvanced();
+    document.querySelector(".advancedSplit").innerHTML = advancedSplit();
+}
+
+//  SCREEN SWITCH CONTROLS
+// SIMPLE
+// turn off the SIMPLE form and display results
+const handleDisplaySimple = () => {
     document.querySelector(".form").style.display = "none";
     document.querySelector(".results").style.display = "block";
 }
-// turn off the results and display the original form
-const goBack = () => {
+// turn off the SIMPLE results and display the original form
+const goBackSimple = () => {
     document.querySelector(".form").style.display = "block";
     document.querySelector(".results").style.display = "none";
     document.querySelector(".cost").value = "";
 }
+// ADVANCED
+// turn off the ADVANCED form and display results
+const handleDisplayAdvanced = () => {
+    document.querySelector(".aForm").style.display = "none";
+    document.querySelector(".aResults").style.display = "block";
+}
+// turn off the ADVANCED results and display the original form
+const goBackAdvanced = () => {
+    document.querySelector(".aForm").style.display = "block";
+    document.querySelector(".aResults").style.display = "none";
+    document.querySelector(".aCost").value = "";
+}
 
+// CALCULATOR SELECTOR CONTROLS
 // go to simple tip calculator
 const simple = () => {
     document.querySelector(".advanced").style.display = "none";
